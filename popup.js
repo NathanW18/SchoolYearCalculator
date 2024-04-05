7c521aa8acdf484ddb3e8d415d2691aebced38fa
NathanW18 <166173082+NathanW18@users.noreply.github.com>
2024-04-05 16:15:53 -0400

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const startDate = new Date('2023-09-05T00:00:00');
+  const endDate = new Date('2024-06-29T00:00:00');
+  const totalTime = endDate - startDate;
+
+  const progressBar = document.getElementById('progressFill');
+  const resultDiv = document.getElementById('result');
+
+const updatePercentage = () => {
+  const currentTime = new Date();
+  const elapsedTime = currentTime - startDate;
+  
+  let percentage = (elapsedTime / totalTime) * 100;
+  percentage = Math.max(0, Math.min(100, percentage));
+
+  progressBar.style.width = percentage + '%';
+  resultDiv.textContent = `Progress: ${percentage.toFixed(2)}%`;
+
+  setTimeout(updatePercentage, 1000);
+};
+
+  // Initial call to update the percentage
+  updatePercentage();
+});
